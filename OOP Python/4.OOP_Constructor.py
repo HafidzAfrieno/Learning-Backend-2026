31da407e83611f1266908568e56a64ee9431d5c2
HafidzAfreieno <hafidzprogramer@gmail.com>
2026-08-06 11:16:08 +0700

    update code oop

---
+++ OOP Python/4.OOP_Constructor.py
@@ -0,0 +1,11 @@
+class Mahasiswa :
+    def __init__(self,nama='',nim='',prodi='',ipk=0.0,sks=0):
+        self.nama = nama
+        self.nim = nim
+        self.prodi = prodi
+
+        self__ipk = ipk
+        self__sks = sks
+
+    def data_diri(self):
+        print()
