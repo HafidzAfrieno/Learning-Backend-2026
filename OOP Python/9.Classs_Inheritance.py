bf92a61be94f574de10a70373bd00618936c0ea0
HafidzAfreieno <hafidzprogramer@gmail.com>
2026-08-07 21:02:33 +0700

    update code OOP

--- OOP Python/9.Classs_Inheritance.py
+++ OOP Python/9.Classs_Inheritance.py
@@ -1,20 +1,29 @@
 class Mahasiswa:
-    def __init__(self,nama,umur):
+    def __init__(self,nama = 'none',umur = '0'):
         self.nama = nama
         self.umur = umur
 
     def tampil_umur(self):
         print(f'Suadara {self.nama} Umurnya adalah {self.umur}')
 
-class Nilai_Mahasiswa(Mahasiswa):
-    def __init__(self,nama,umur,ipk):
-        super().__init__(nama,umur)
+class Dosen_Wali(Mahasiswa):
+    def __init__(self,dosen,**kwargs):
+        super().__init__(**kwargs)
+        self.dosen = dosen
+
+    def tampil_dosen(self):
+        print(f'Dosen Wali Saudara {self.nama} Bernama {self.dosen}')
+
+class Nilai_Mahasiswa(Dosen_Wali,Mahasiswa):
+    def __init__(self,ipk,nama,umur,dosen):
+        super().__init__(nama=nama,umur=umur,dosen=dosen)
         self.ipk = ipk
 
     def tampil_nilai(self):
-        print('='*20)
-        self.tampil_umur()
         print(f'Saudara {self.nama} dan umurnya {self.umur} mendapatkan IPK {self.ipk}')
 
-IPk_mahasiswa_1 = Nilai_Mahasiswa('Hafidz Akmal',20,3.8)
+IPk_mahasiswa_1 = Nilai_Mahasiswa('Hafidz Akmal',20,3.8,'Eko')
+
+IPk_mahasiswa_1.tampil_umur()
 IPk_mahasiswa_1.tampil_nilai()
+IPk_mahasiswa_1.tampil_dosen()
