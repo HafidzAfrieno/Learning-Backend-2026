7cc7f081ad2012f486ada1d0d86d7f9323bf8075
HafidzAfreieno <hafidzprogramer@gmail.com>
2026-08-22 20:22:29 +0700

    update code

--- Mini_Project/3.Expense_Tracker_App/Expense_Tracker.py
+++ Mini_Project/3.Expense_Tracker_App/Expense_Tracker.py
@@ -3,3 +3,18 @@ import argparse
 import uuid
 from os import path
 from datetime import datetime
+
+class InputData:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="APP TASK TRACKER")
+        self.subparser = self.parser.add_subparsers(dest="command", help="Perintah Yang Tersedia")
+        self.init_parsers()
+
+    def init_parsers(self):
+        print("hallo")
+
+def main():
+    print()
+
+if __name__ == "__main__":
+    main()
