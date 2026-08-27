2e541a4069090252d82cf42b6cd741602f1b6a3c
HafidzAfreieno <hafidzprogramer@gmail.com>
2026-08-27 10:56:29 +0700

    update code expense tracker

--- Mini_Project/3.Expense_Tracker_App/Expense_Tracker.py
+++ Mini_Project/3.Expense_Tracker_App/Expense_Tracker.py
@@ -31,14 +31,74 @@ class InputData:
         self.args = self.parser.parse_args()
         return self.args
 
+class JsonFileHandler(InputData):
+    def __init__(self):
+        super().__init__()
+        self.id = ""
+        self.descriptions = ""
+        self.amount = 0
+        self.date = ""
+        self.fileName = "data.json"
 
-    class ExpenseCalculator:
-        def __init__(self):
-            self.nominal = 0
-            self.mounth = ""
+    def openFilejson(self):
+        task_list = []
+        if path.exists(self.fileName):
+            try:
+                with open(self.fileName, "r", encoding="utf-8") as file:
+                    data = json.load(file)
+                    if isinstance(data, list):
+                        task_list = data
+                    elif isinstance(data, dict):
+                        task_list = [data]
+            except json.JSONDecodeError:
+                task_list = []
+        return task_list
+
+    def create_data(self, parsed_argumens):
+        task_id = str(uuid.uuid4())[:8]
+        created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        self.id = task_id
+        self.descriptions = parsed_argumens.description
+        self.amount = self.parse_arguments.amount
+        self.date = created_at
+
+        new_task = {
+            "id"           : self.id,
+            "descriptions" : self.descriptions,
+            "Amount"       : self.amount,
+            "Date"         : self.date
+        }
+
+        task_list = self.openFilejson()
+        task_list.append(new_task)
+        with open(self.fileName, "w", encoding="utf-8") as file:
+            json.dump(task_list, file, indent=4, ensure_ascii=False)
+        print(f"Berhasil menambahkan dan menyimpan Pengeluaran (ID: {task_id})!")
+
+    def list_data(self, parsed_argumens):
+        expense_list = self.openFilejson()
+        
+        
+            
+
+class ExpenseCalculator:
+    def __init__(self):
+        self.nominal = 0
+        self.date = datetime()
+        self.id = ""
+
+    def sum_all_expense(self):
+        print()
+
+
+        
+
+        
 
 def main():
-    print()
+    app = JsonFileHandler()
+    parsed_args = app.parse_arguments()
 
 if __name__ == "__main__":
     main()
