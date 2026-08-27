9ca77608c90f7a6c62356665ac467af8b7946276
HafidzAfreieno <hafidzprogramer@gmail.com>
2026-08-27 21:47:23 +0700

    firts commit code game

--- Mini_Project/4.Number_Guessing_Game/Number_Guessing.py
+++ Mini_Project/4.Number_Guessing_Game/Number_Guessing.py
@@ -32,8 +32,7 @@ class GameController:
             return None, None
         return chances, name_level
     
-    
-    def play_game(self,level):
+    def play_game(self, level):
         chances, name_level = self.chances_game(level)
         if chances is None:
             return
@@ -41,19 +40,27 @@ class GameController:
         print(f"Great! You have selected the {name_level} difficulty level.")
         print("Let's start the game!\n")
 
-        secret_number = random.randint(self.first_number, self.last_number)
-        for attempt in range(1, chances + 1):
-            input_number = int(input(f"Enter your guess ({self.first_number}-{self.last_number}): "))
-            if input_number == secret_number:
-                print(f"Enter your guess: {input_number}")
-                print(f"Congratulations! You guessed the correct number in {attempt} attempts.\n")
-                return
-            elif input_number > secret_number:
-                print(f"Enter your guess: {input_number}")
-                print(f"Incorrect! The number is less than {input_number}.\n")
-            else:
-                print(f"Enter your guess: {input_number}")
-                print(f"Incorrect! The number is greater than {input_number}.\n")
+        next_game = "start"
+        while next_game.lower() != "stop":
+            secret_number = random.randint(self.first_number, self.last_number)
+            guessed_correctly = False
+
+            for attempt in range(1, chances + 1):
+                input_number = int(input(f"Enter your guess ({self.first_number}-{self.last_number}): "))
+                
+                if input_number == secret_number:
+                    print(f"Congratulations! You guessed the correct number in {attempt} attempts.\n")
+                    guessed_correctly = True
+                    break 
+                elif input_number > secret_number:
+                    print(f"Incorrect! The number is less than {input_number}.\n")
+                else:
+                    print(f"Incorrect! The number is greater than {input_number}.\n")
+            
+            if not guessed_correctly:
+                print(f"Game Over! The correct number is {secret_number}.\n")
+            next_game = input("Do you want to continue or not? (type 'stop' to quit): ")
+            print()
 
 def main():
     NumGues = GameController()
